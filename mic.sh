075948d5b3bc2c3ed31e6716cbf619051ea9fb52
SzymeczeqTechManieczeq <68206764+SzymeczeqTechManieczeq@users.noreply.github.com>
2021-02-08 20:04:31 +0100

    Delete mic.sh

--- mic.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo chmod +x micclient-x86_64.AppImage
-sudo modprobe snd-aloop
-read -p "Enter your phone IP (connect by WiFi is working now only!): "  wifi
-./micclient-x86_64.AppImage -t Wifi $wifi
